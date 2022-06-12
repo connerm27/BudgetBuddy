be02ce8c98c2c6a5958575bb525b5d7af27de093
Conner Montgomery <cmontgomery2799@gmail.com>
2022-06-12 14:01:56 -0400

    add file

---
+++ BudgetBuddy/BudgetCategory.swift
@@ -0,0 +1,21 @@
+//
+//  BudgetCategory.swift
+//  BudgetBuddy
+//
+//  Created by Conner Montgomery on 6/12/22.
+//
+
+import Foundation
+
+// Budget Category -- Class
+class BudgetCategory {
+    
+    var category: String
+    var amount: String
+    
+    init(category:String, amount:String) {
+        self.category = category
+        self.amount = amount
+    }
+    
+}
